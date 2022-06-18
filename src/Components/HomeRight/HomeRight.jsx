6d29a7664eb7e8db643d9e9fae19f8b3d95372fd
david <deividasmataitis@gmail.com>
2022-06-18 22:29:44 +0300

    added home right

--- src/Components/HomeRight/HomeRight.jsx
+++ src/Components/HomeRight/HomeRight.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import styles from './homeright.module.scss';
+import crunchy from '../../Assets/crunchyroll.png';
+import funi from '../../Assets/funimation.png';
 
 const HomeRight = () => {
   return (
     <div className={styles.main}>
       <h1>Where you can watch Anime?</h1>
+      <div>
+        <img src={crunchy} alt="crunchy" />
+        <a href="https://www.crunchyroll.com" target="_blank" >Crunchyroll</a>
+      </div>
+      <div>
+        <img src={funi} alt="funi" />
+        <a href="https://www.funimation.com" target="_blank" >Funimation</a>
+      </div>
+      
+
+
     </div>
   )
 }
